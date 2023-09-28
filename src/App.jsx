678626e7b2a6695df37832f6f598ee0711faec26
upgraded57 <shokunbiupgraded@gmail.com>
2023-09-28 14:54:45 +0100

    Development underway

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loader from "./Components/Loader/Loader";
 const Onboarding = React.lazy(() => import("./Pages/Onboarding/Onboarding"));
+const Home = React.lazy(() => import("./Pages/Home/Home"));
 
 function App() {
   return (
@@ -12,6 +13,14 @@ function App() {
           element={
             <Suspense fallback={<Loader />}>
               <Onboarding />
+            </Suspense>
+          }
+        />
+        <Route
+          index
+          element={
+            <Suspense fallback={<Loader />}>
+              <Home />
             </Suspense>
           }
         />
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import "./home.css";
+import logo from "../../../public/images/logo.svg";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <img src={logo} alt="" />
+    </div>
+  );
+}
