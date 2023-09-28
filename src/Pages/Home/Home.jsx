981c637f1a6a0832bce0ae85110d53c0e5f6a8cd
upgraded57 <shokunbiupgraded@gmail.com>
2023-09-28 19:46:44 +0100

    Fixed home bug

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,5 @@
 import "./home.css";
-import logo from "../../../public/images/logo.png";
 
 export default function Home() {
-  return (
-    <div className="home">
-      <div className="home__logo">
-        <img src={logo} alt="" />
-      </div>
-    </div>
-  );
+  return <div className="home">Home</div>;
 }
