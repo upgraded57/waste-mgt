c648e67ac85ccdb8e7f65aa6d70dbe1e7d86c6bf
upgraded57 <shokunbiupgraded@gmail.com>
2023-09-30 13:19:23 +0100

    Added vercel manual routing

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,12 @@
 import "./home.css";
+import logo from "../../assets/images/logo.png";
 
 export default function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="home__logo">
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  );
 }
